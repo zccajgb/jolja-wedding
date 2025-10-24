14fd5ff5e7c15dde070d897ace7a35c106ad00f3
zccajgb <josephgbuckley@gmail.com>
2025-10-24 21:31:15 +0100

    fix redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ export default defineConfig({
     defaultLocale: 'en',
     locales: ['en', 'ru'],
     routing: {
-      prefixDefaultLocale: true  // URLs will be /en and /vi
+     // prefixDefaultLocale: true,  // URLs will be /en and /vi
+      redirectToDefaultLocal: false
     }
   },
   vite: {
