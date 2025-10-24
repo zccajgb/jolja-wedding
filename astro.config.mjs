4cd7c8a72ba4effd4f59b96dd0f7e88389c6084c
zccajgb <josephgbuckley@gmail.com>
2025-10-24 22:10:06 +0100

    fix redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({  
+  output: "static",
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
