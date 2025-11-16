0b2900288a033080c4cebab6df2b93890a501bee
zccajgb <josephgbuckley@gmail.com>
2025-11-16 14:29:55 +0000

    remove russian site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   output: "static",
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ru'],
+    locales: ['en'],//, 'ru'],
     routing: {
       //prefixDefaultLocale: true,  // URLs will be /en and /vi
       redirectToDefaultLocal: false
